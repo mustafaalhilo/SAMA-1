c925e0807a5773b7f911e88d744b0e904c2fabc3
Christopher B. Barnwell <christophersbarnandwell@gmail.com>
2019-04-11 20:08:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MovieAdapter.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MovieAdapter extends ArrayAdapter<Movie> {
+    private Context mContext;
+    private List<Movie> moviesList = new ArrayList<>();
+
+    public MovieAdapter(@NonNull Context context,@LayoutRes ArrayList<Movie> list) {
+        super(context, 0 , list);
+        mContext = context;
+        moviesList = list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item,parent,false);
+
+        Movie currentMovie = moviesList.get(position);
+
+
+        TextView name = listItem.findViewById(R.id.textView_name);
+        name.setText(currentMovie.getmName());
+
+        TextView release = listItem.findViewById(R.id.textView_release);
+        release.setText(currentMovie.getmRelease());
+
+        return listItem;
+    }
+}
